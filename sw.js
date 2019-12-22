ce85046264be30345ab1c800c5b3b83586276e57
nexus-uw <nexus-uw@users.noreply.github.com>
2019-12-22 02:04:42 +0000

    Deploy to GitHub pages

--- sw.js
+++ sw.js
@@ -2,10 +2,6 @@ importScripts('/_nuxt/workbox.6041bd81.js', 'custom-service-worker.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/0e9d44eb0c26b904f910.js",
-    "revision": "3daa8425fa1540698bd300f824153072"
-  },
-  {
     "url": "/_nuxt/1a615f7547c9b895ca8f.css",
     "revision": "74a3e8fa9ffc98c4abd4aeaaf7a4b349"
   },
@@ -22,36 +18,40 @@ workbox.precaching.precacheAndRoute([
     "revision": "fc9193480c929d4b0808e0210347e4df"
   },
   {
-    "url": "/_nuxt/5139a60b9ba50b352a4f.js",
-    "revision": "2af5b27fccce0babf8016712fb6d01ee"
+    "url": "/_nuxt/3f9780b04dfb709ee67d.js",
+    "revision": "909e61bb1566b45472d0c4e2b9b3fcc2"
+  },
+  {
+    "url": "/_nuxt/488cb8fcc57c39c9ad5c.js",
+    "revision": "157602c43870147e23094b085722b2c4"
   },
   {
     "url": "/_nuxt/6b192442b795157ffaf9.js",
     "revision": "84ee440d8ad985a6da3eebcb14c5a6ae"
   },
   {
-    "url": "/_nuxt/744ce7de433d3ba5c6c6.js",
-    "revision": "c97408f348948da1be68179435645ebc"
-  },
-  {
-    "url": "/_nuxt/7897aef57cb20c692ca0.js",
-    "revision": "1c079c5ae6ec1cd9a182538a48e9b1c3"
+    "url": "/_nuxt/7f745a7c1bebbe856298.js",
+    "revision": "c4068d5bd0d641abcd66b6458f559975"
   },
   {
     "url": "/_nuxt/acc3b8ba1550c5a968a7.css",
     "revision": "e4d51be615c59b49968a6e1443ff40e6"
   },
   {
-    "url": "/_nuxt/ae9383b7c4a4051d1148.js",
-    "revision": "572b75b716774af1cc0100ac9b5d8a8e"
-  },
-  {
     "url": "/_nuxt/b63bf39703b2798b9b76.js",
     "revision": "6f70c346430bd68fd4faacb00748ccbb"
   },
   {
+    "url": "/_nuxt/c44baacfbd4ee619011d.js",
+    "revision": "fb3460f845a79fdea85263854d0fbe11"
+  },
+  {
     "url": "/_nuxt/e3b0e5c3de839a599300.js",
     "revision": "2f95b1d60cba8765b6a6bd11757b129c"
+  },
+  {
+    "url": "/_nuxt/e71ebaea5d42b85ab470.js",
+    "revision": "5a9823ddf72044a0825f819f9254ebed"
   },
   {
     "url": "/_nuxt/e8f8e91af8d421bab18a.js",
