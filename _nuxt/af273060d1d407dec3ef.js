0c359060284118b739fdeb36da89342ab3772c1d
nexus-uw <nexus-uw@users.noreply.github.com>
2020-03-25 00:05:13 +0000

    Deploy to GitHub pages

---
+++ _nuxt/af273060d1d407dec3ef.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{340:function(n,t,e){"use strict";e.r(t);var l=e(17),component=Object(l.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("PONG")])}),[],!1,null,null,null);t.default=component.exports}}]);
