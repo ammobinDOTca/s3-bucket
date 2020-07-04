3cde90c1a492ae16681e04e0b40c037d6c2fae84
nexus-uw <nexus-uw@users.noreply.github.com>
2020-07-04 00:08:37 +0000

    Deploy to GitHub pages

---
+++ _nuxt/pages/ping.624b92a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{245:function(n,t,e){"use strict";e.r(t);var l=e(28),component=Object(l.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("PONG")])}),[],!1,null,null,null);t.default=component.exports}}]);
