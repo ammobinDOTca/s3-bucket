895b1abecc0e4e2885e3f6eb0b5bf868158a6c82
nexus-uw <nexus-uw@users.noreply.github.com>
2021-02-28 15:15:37 +0000

    Deploy to GitHub pages

---
+++ _nuxt/46f251e.modern.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{266:function(n,t,e){"use strict";e.r(t);var l=e(29),component=Object(l.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("PONG")])}),[],!1,null,null,null);t.default=component.exports}}]);
---
+++ _nuxt/static/1614525194/en/case/powder/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1614525194",layout:"default",error:a,state:{isCrawler:false,locales:[b,"fr"],locale:b,graphql:{vendors:a,flatItems:new Map([])}},serverRendered:true,routePath:"\u002Fen\u002Fcase\u002Fpowder",config:{}}}(null,"en"));
