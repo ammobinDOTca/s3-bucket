b5782d656dbf64f850723944cd787df3b981de33
nexus-uw <nexus-uw@users.noreply.github.com>
2021-03-03 15:21:19 +0000

    Deploy to GitHub pages

---
+++ _nuxt/static/1614784738/en/reloading/UNKNOWN/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1614784738",layout:"default",error:a,state:{isCrawler:false,locales:[b,"fr"],locale:b,graphql:{vendors:a,flatItems:new Map([])}},serverRendered:true,routePath:"\u002Fen\u002Freloading\u002FUNKNOWN",config:{}}}(null,"en"));
