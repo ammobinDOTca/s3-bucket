f0f203f47ffd38749c4d2f59f01ea9037743efac
nexus-uw <nexus-uw@users.noreply.github.com>
2021-03-11 15:21:33 +0000

    Deploy to GitHub pages

---
+++ _nuxt/static/1615475951/en/case/case/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615475951",layout:"default",error:a,state:{isCrawler:false,locales:[b,"fr"],locale:b,graphql:{vendors:a,flatItems:new Map([])}},serverRendered:true,routePath:"\u002Fen\u002Fcase\u002Fcase",config:{}}}(null,"en"));
---
+++ _nuxt/static/1615475951/fr/rimfire/.22%20WMR/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615475951",layout:"default",error:a,state:{isCrawler:false,locales:["en",b],locale:b,graphql:{vendors:a,flatItems:new Map([])}},serverRendered:true,routePath:"\u002Ffr\u002Frimfire\u002F.22%20WMR",config:{}}}(null,"fr"));
