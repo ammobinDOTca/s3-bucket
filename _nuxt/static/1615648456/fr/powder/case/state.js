54230b4acdcb00610436a6fae41341c3bdfddb88
nexus-uw <nexus-uw@users.noreply.github.com>
2021-03-13 15:16:38 +0000

    Deploy to GitHub pages

---
+++ _nuxt/static/1615648456/fr/powder/case/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615648456",layout:"default",error:a,state:{isCrawler:false,locales:["en",b],locale:b,graphql:{vendors:a,flatItems:new Map([])}},serverRendered:true,routePath:"\u002Ffr\u002Fpowder\u002Fcase",config:{}}}(null,"fr"));
