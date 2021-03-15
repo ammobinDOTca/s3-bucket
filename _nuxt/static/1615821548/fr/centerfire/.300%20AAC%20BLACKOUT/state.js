47e833da4096e55c46c1b5b7e8dc82d032e65267
nexus-uw <nexus-uw@users.noreply.github.com>
2021-03-15 15:21:31 +0000

    Deploy to GitHub pages

---
+++ _nuxt/static/1615821548/fr/centerfire/.300%20AAC%20BLACKOUT/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615821548",layout:"default",error:a,state:{isCrawler:false,locales:["en",b],locale:b,graphql:{vendors:a,flatItems:new Map([])}},serverRendered:true,routePath:"\u002Ffr\u002Fcenterfire\u002F.300%20AAC%20BLACKOUT",config:{}}}(null,"fr"));
