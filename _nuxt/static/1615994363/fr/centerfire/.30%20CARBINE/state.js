0ff0c93b10bd07399e0b0eca18c7e5bacde68d57
nexus-uw <nexus-uw@users.noreply.github.com>
2021-03-17 15:21:45 +0000

    Deploy to GitHub pages

---
+++ _nuxt/static/1615994363/fr/centerfire/.30%20CARBINE/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1615994363",layout:"default",error:a,state:{isCrawler:false,locales:["en",b],locale:b,graphql:{vendors:a,flatItems:new Map([])}},serverRendered:true,routePath:"\u002Ffr\u002Fcenterfire\u002F.30%20CARBINE",config:{}}}(null,"fr"));
