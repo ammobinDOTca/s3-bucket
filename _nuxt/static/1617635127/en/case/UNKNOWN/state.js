ea609ebad2c47826ff4ee0ce3bb123f43fddd53a
nexus-uw <nexus-uw@users.noreply.github.com>
2021-04-05 15:07:50 +0000

    Deploy to GitHub pages

---
+++ _nuxt/static/1617635127/en/case/UNKNOWN/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1617635127",layout:"default",error:a,state:{isCrawler:false,locales:[b,"fr"],locale:b,graphql:{vendors:a,flatItems:new Map([])}},serverRendered:true,routePath:"\u002Fen\u002Fcase\u002FUNKNOWN",config:{}}}(null,"en"));
---
+++ _nuxt/static/1617635127/fr/centerfire/.44%20S&W%20SPECIAL/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1617635127",layout:"default",error:a,state:{isCrawler:false,locales:["en",b],locale:b,graphql:{vendors:a,flatItems:new Map([])}},serverRendered:true,routePath:"\u002Ffr\u002Fcenterfire\u002F.44%20S&W%20SPECIAL",config:{}}}(null,"fr"));
