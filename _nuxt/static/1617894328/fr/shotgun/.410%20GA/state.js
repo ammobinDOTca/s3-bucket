9d307a0957342d40655c2eab0b897d225505f351
nexus-uw <nexus-uw@users.noreply.github.com>
2021-04-08 15:07:52 +0000

    Deploy to GitHub pages

---
+++ _nuxt/static/1617894328/fr/shotgun/.410%20GA/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1617894328",layout:"default",error:a,state:{isCrawler:false,locales:["en",b],locale:b,graphql:{vendors:a,flatItems:new Map([])}},serverRendered:true,routePath:"\u002Ffr\u002Fshotgun\u002F.410%20GA",config:{}}}(null,"fr"));
