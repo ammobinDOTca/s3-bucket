7249262073d15005c32bf61c7263e07a849d5885
nexus-uw <nexus-uw@users.noreply.github.com>
2021-04-15 15:07:42 +0000

    Deploy to GitHub pages

---
+++ _nuxt/static/1618499115/en/centerfire/.45-70/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1618499115",layout:"default",error:a,state:{isCrawler:false,locales:[b,"fr"],locale:b,graphql:{vendors:a,flatItems:new Map([])}},serverRendered:true,routePath:"\u002Fen\u002Fcenterfire\u002F.45-70",config:{}}}(null,"en"));
