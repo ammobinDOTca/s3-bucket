2b443baa946747337e1ec2f3afe3de9e39db70a5
nexus-uw <nexus-uw@users.noreply.github.com>
2021-05-11 15:19:38 +0000

    Deploy to GitHub pages

---
+++ _nuxt/static/1620746233/fr/centerfire/.38%20LONG%20COLT/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620746233",layout:"default",error:a,state:{isCrawler:false,locales:["en",b],locale:b,graphql:{vendors:a,flatItems:new Map([])}},serverRendered:true,routePath:"\u002Ffr\u002Fcenterfire\u002F.38%20LONG%20COLT",config:{}}}(null,"fr"));
