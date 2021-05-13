9378f95d63e6bf045893660d92b14fe094d7d52f
nexus-uw <nexus-uw@users.noreply.github.com>
2021-05-13 15:20:53 +0000

    Deploy to GitHub pages

---
+++ _nuxt/static/1620919109/en/rimfire/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1620919109",layout:"default",error:a,state:{isCrawler:false,locales:[b,"fr"],locale:b,graphql:{vendors:a,flatItems:new Map([])}},serverRendered:true,routePath:"\u002Fen\u002Frimfire",config:{}}}(null,"en"));
