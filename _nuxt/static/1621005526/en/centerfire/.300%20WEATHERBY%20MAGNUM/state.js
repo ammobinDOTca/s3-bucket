e7414979c2111ba665410013e00457c2947583f2
nexus-uw <nexus-uw@users.noreply.github.com>
2021-05-14 15:21:11 +0000

    Deploy to GitHub pages

---
+++ _nuxt/static/1621005526/en/centerfire/.300%20WEATHERBY%20MAGNUM/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1621005526",layout:"default",error:a,state:{isCrawler:false,locales:[b,"fr"],locale:b,graphql:{vendors:a,flatItems:new Map([])}},serverRendered:true,routePath:"\u002Fen\u002Fcenterfire\u002F.300%20WEATHERBY%20MAGNUM",config:{}}}(null,"en"));
