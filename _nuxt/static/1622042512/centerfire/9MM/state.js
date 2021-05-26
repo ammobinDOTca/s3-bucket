7e61fa068071faf70b0a0f789d1a85b5532642fe
nexus-uw <nexus-uw@users.noreply.github.com>
2021-05-26 15:24:18 +0000

    Deploy to GitHub pages

---
+++ _nuxt/static/1622042512/centerfire/9MM/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1622042512",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fcenterfire\u002F9MM",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
