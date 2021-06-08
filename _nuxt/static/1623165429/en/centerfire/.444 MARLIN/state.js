8bc3945fb313768a390a89e93292429c52689d4c
nexus-uw <nexus-uw@users.noreply.github.com>
2021-06-08 15:19:36 +0000

    Deploy to GitHub pages

---
+++ _nuxt/static/1623165429/en/centerfire/.444 MARLIN/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623165429",layout:"default",error:a,state:{isCrawler:false,locales:[b,"fr"],locale:b,graphql:{vendors:a,flatItems:new Map([])}},serverRendered:true,routePath:"\u002Fen\u002Fcenterfire\u002F.444%20MARLIN",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"en"));
