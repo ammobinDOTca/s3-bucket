85eceb073b853930ce2c33516852f39bc4595514
nexus-uw <nexus-uw@users.noreply.github.com>
2021-06-17 15:19:19 +0000

    Deploy to GitHub pages

---
+++ _nuxt/static/1623943014/en/centerfire/.222 REMINGTON/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623943014",layout:"default",error:a,state:{isCrawler:false,locales:[b,"fr"],locale:b,graphql:{vendors:a,flatItems:new Map([])}},serverRendered:true,routePath:"\u002Fen\u002Fcenterfire\u002F.222%20REMINGTON",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"en"));
