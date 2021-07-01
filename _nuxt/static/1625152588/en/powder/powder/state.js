b28dee8233df74df3312126460325c70de1736f7
nexus-uw <nexus-uw@users.noreply.github.com>
2021-07-01 15:18:55 +0000

    Deploy to GitHub pages

---
+++ _nuxt/static/1625152588/en/powder/powder/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625152588",layout:"default",error:a,state:{isCrawler:false,locales:[b,"fr"],locale:b,graphql:{vendors:a,flatItems:new Map([])}},serverRendered:true,routePath:"\u002Fen\u002Fpowder\u002Fpowder",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"en"));
