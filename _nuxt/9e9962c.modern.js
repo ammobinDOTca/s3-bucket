460895a63ef80d61b349670bede7dc9ef853f50e
nexus-uw <nexus-uw@users.noreply.github.com>
2021-07-07 15:22:35 +0000

    Deploy to GitHub pages

---
+++ _nuxt/9e9962c.modern.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{271:function(n,e,t){"use strict";t.r(e);var l=t(34),component=Object(l.a)({},(function(){var n=this,e=n.$createElement;return(n._self._c||e)("div",[n._v("PONG")])}),[],!1,null,null,null);e.default=component.exports}}]);
---
+++ _nuxt/static/1625671206/centerfire/.300 WEATHERBY MAGNUM/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1625671206",layout:"default",error:a,state:a,serverRendered:false,routePath:"\u002Fcenterfire\u002F.300%20WEATHERBY%20MAGNUM",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
