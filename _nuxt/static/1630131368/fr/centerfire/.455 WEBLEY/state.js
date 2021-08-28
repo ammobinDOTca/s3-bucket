4b9ca6708c644b0aa854d2e938a65fa0369f8d4e
nexus-uw <nexus-uw@users.noreply.github.com>
2021-08-28 06:18:14 +0000

    Deploy to GitHub pages

---
+++ _nuxt/static/1630131368/fr/centerfire/.455 WEBLEY/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630131368",layout:"default",error:a,state:{isCrawler:false,locales:["en",b],locale:b,graphql:{vendors:a,flatItems:new Map([])}},serverRendered:true,routePath:"\u002Ffr\u002Fcenterfire\u002F.455%20WEBLEY",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"fr"));
