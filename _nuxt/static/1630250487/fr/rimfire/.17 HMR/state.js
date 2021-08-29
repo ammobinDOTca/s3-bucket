925e226aed5c58b110c5aae0324b936dbdb73fdb
nexus-uw <nexus-uw@users.noreply.github.com>
2021-08-29 15:23:26 +0000

    Deploy to GitHub pages

---
+++ _nuxt/static/1630250487/fr/rimfire/.17 HMR/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630250487",layout:"default",error:a,state:{isCrawler:false,locales:["en",b],locale:b,graphql:{vendors:a,flatItems:new Map([])}},serverRendered:true,routePath:"\u002Ffr\u002Frimfire\u002F.17%20HMR",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"fr"));
